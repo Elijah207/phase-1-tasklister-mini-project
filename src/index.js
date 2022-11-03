2e0a80e7a03d47cb47c9040f1163f842dd868da4
elijah207 <elijahvanfuller@gmail.com>
2022-11-03 15:57:16 -0700

    saving solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // STEPS
+  // indentify what we want ot work with on DOM
+  // establish some constants to save us time
+  const createTaskForm = document.querySelector
+  (`#create-task-form`)
+  const newTaskDescription = document.querySelector
+    (`#new-task-description`)
+  
+
+  createTaskForm.addEventListener(`submit`, (event) => {
+    event.preventDefault()
+  
+    const newTaskDescription = document.querySelector
+    (`#new-task-description`)
+    console.log(newTaskDescription.value)
+    
+
+    const ul = document.querySelector("#tasks")
+    const newTask = document.createElement(`li`)
+    
+    newTask.textContent = newTaskDescription.value
+    
+    ul.append(newTask)
+  })
+
 });
